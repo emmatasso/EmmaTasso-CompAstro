c6162f21f22d1fd1458e5f236e744d5716eaef45
emmatasso <157404269+emmatasso@users.noreply.github.com>
2025-09-02 14:37:51 -0400

    Update and rename argparse.py to falltime.py

--- falltime.py
+++ falltime.py
@@ -7,5 +7,5 @@ parser = argparse.ArgumentParser(description="Compute fall time from height h.")
 parser.add_argument("h", type=float, help="height (m)")
 parser.add_argument("-g", "--gravity", type=float, default=9.8, help="gravity (m/s^2)")
 
-args = parser.parse_args(args=["20", "-g", "9.8"])
+args = parser.parse_args()
 print("time =", balltime(args.h, args.gravity), "s")
